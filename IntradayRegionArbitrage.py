f4af583bc4c195862322eda557c604526ac2d32a
oliverweissl <o.weissl@student.vu.nl>
2023-01-24 23:15:59 +0100

    upd --> cascade backtest WIP

--- IntradayRegionArbitrage.py
+++ IntradayRegionArbitrage.py
@@ -21,24 +21,27 @@ class IntradayRegionArbitrage:
 
         self._tickers: list = []
         self._price_data: list = []
+        self._base_share_price: float = 0
         self.cpt: list = []  # current position tickers
 
     def data_feed(self, timestamp: time, return_data: list, price_data: list, tickers: list):
-        self._price_data = price_data
+        assert len(tickers) == len(return_data), "Tickers and Data length dont match"
+        self._tickers = tickers
+        self._base_share_price = price_data[0]
+        assert len(tickers) == len(price_data[1:]), "Tickers and Price length dont match"
+        self._price_data = price_data[1:]
 
         is_closing = self.is_closing(timestamp)
-
         if self.in_trade:
             self.price_check(return_data)
             if is_closing:
                 self.close_trades()
         elif (not self.in_trade) and (not is_closing):
             opportunity, signal = self.check_opportunity(return_data)
-            self._tickers = [tickers[i+1] for i in np.where(opportunity)[0].tolist()]  # +1 because tickers has base ticker in it
-            if len(signal := [signal[i] for i in np.where(opportunity)[0].tolist()]) > 0:
+            self.cpt = np.where(opportunity)[0].tolist()
+
+            if len(self.cpt) > 0:
                 self.trade_signal(signal)
-
-
 
     def check_opportunity(self, return_data: list) -> (list, list):
         opportunity, signal = [], []
@@ -48,40 +51,52 @@ class IntradayRegionArbitrage:
         return opportunity, signal
 
     def trade_signal(self, signal: list):
-        order_size = (self.trade_size * self.balance if self.trade_size_percent else self.trade_size) / len(signal) * 2
-        for i, ticker in enumerate(self._tickers):
-            self.shares[ticker] = order_size / self._price_data[i] * (1 if signal[i] else -1)
-            self.base_shares[ticker] = order_size / self._price_data[0] * (-1 if signal[i] else 1)
+        order_size = (self.trade_size * self.balance if self.trade_size_percent else self.trade_size) / len(self.cpt) * 2
+        for i in self.cpt:
+            self.shares[self._tickers[i]] = order_size / self._price_data[i] * (1 if signal[i] else -1)
+            self.base_shares[self._tickers[i]] = order_size / self._base_share_price * (-1 if signal[i] else 1)
+
+            print(f"getting {self._tickers[i]}: Amt-> {order_size / self._price_data[i]} @{self._price_data[i]}")
+            print(f"getting BaseShare: Amt-> {order_size / self._base_share_price} @{self._base_share_price}")
         self.in_trade = True
 
     def price_check(self, return_data: list):
-        for i, ticker in enumerate(self._tickers):
+        for i in self.cpt:
             # trigger take profit
             if (return_data[i] <= self.trigger_range) and (return_data[i] >= -self.trigger_range):
-                self.close_trade(i, ticker)
+                self.close_trade(i)
                 break
 
             # trigger stop-loss
-            pnl = self.shares[ticker] * self._price_data[i] + self.base_shares[ticker] * self._price_data[0]
+            pnl = self.shares[self._tickers[i]] * self._price_data[i] + self.base_shares[self._tickers[i]] * self._price_data[0]
             if pnl < (self.trade_size / len(self._tickers) * self.sl_percent * -1):
-                self.close_trade(i, ticker)
+                self.close_trade(i, self._tickers[i])
 
 
-    def close_trade(self, i: int, ticker: str):
-        self.balance += self.base_shares[ticker] * self._price_data[0]
+    def close_trade(self, i: int):  
+        ticker: str = self._tickers[i]
+        print(f"Balance: {self.balance}")
+        print(f"BaseShare-> amt: {self.base_shares[ticker]} @{self._base_share_price} --> total: {self.base_shares[ticker] * self._base_share_price}")
+        self.balance += self.base_shares[ticker] * self._base_share_price
+
+        print(f"Ticker {ticker}-> amt: {self.shares[ticker]} @{self._price_data[i]} --> total: {self.shares[ticker] * self._price_data[i]}")
         self.balance += self.shares[ticker] * self._price_data[i]
+
+        print(f"Updated-Balance: {self.balance}")
 
         del self.shares[ticker]
         del self.base_shares[ticker]
-        self._tickers.remove(ticker)
 
+        self._tickers.remove(ticker)
+        self.cpt.remove(i)
 
         if len(self._tickers) < 1:
             self.in_trade = False
 
 
     def close_trades(self):
-        [self.close_trade(i, ticker) for i, ticker in enumerate(self._tickers)]
+        if len(self.cpt) > 0:
+            [self.close_trade(i, ticker) for i, ticker in enumerate(self._tickers)]
 
     def is_closing(self, timestamp: time) -> bool:
         return timestamp > time(16, 28, 0)
